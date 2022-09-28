6367802cdb6731250303a914ab425759466ec09e
Steven Mauricio Nunez Murillo <B95614@ecci.ucr.ac.cr>
2022-09-27 18:47:25 -0600

    lab03

--- Lab03/src/predict.py
+++ Lab03/src/predict.py
@@ -10,12 +10,13 @@ import random
 def MSE(y_true, y_predict):
     
     error = 0
-    
+
     for index in range(len(y_true)):
-        error += ((y_true[index] - y_predict[index])**2)
+        error += ((y_predict[index] - y_true[index])**2)
     
     error *= 1/ len(y_true)
     
+    return error
     #mse = mean_squared_error(y_true, y_predict)
     
   
@@ -31,48 +32,101 @@ def score(y_true, y_predict):
     score_r2 = (second_sum-first_sum)/second_sum
     #r2 = r2_score(y_true, y_predict) 
         
-    print(score_r2)
+    #print(score_r2)
 
-def fit(self, x, y, max_epochs=100, threshold=0.01, learning_rate=0.001, momentum=0, decay=0, error='mse', regularization='none', lambdas = 0):
-    coeficients = []
+def fit(x, y, max_epochs=100, threshold=0.01, learning_rate=0.001, momentum=0, decay=0, error='mse', regularization='none', lambdas = 0):
+    
+    #Se agrega el bias  
+    x.insert(0, "ones", 1, allow_duplicates=False)
+
+    #Se pasa de dataframe a matriz numpy
+    x_matrix = x.to_numpy()
+    coeficients = np.array([random.sample(range(len(x_matrix[0])),len(x_matrix[0]))])
     epoch = 0
-    error_change = []
+    epoch_error = []
     
-    df.insert(0, "ones", 1, allow_duplicates=False)
+    coeficients = np.transpose(coeficients)
     
-    data_x = x.to_numpy()
-    ones = np.ones(np.shape(data_x)[0], 1)
-    x_ones = np.concatenate((ones, data_x), axis=0)
-    
-    
-    for elements in range(len(y)):
-        coeficient = random()
-        if coeficient not in coeficients:
-            coeficients.append(coeficient)
-    
+    #Algoritmo de RG
     while epoch < max_epochs:
+
+        xc_pred = xc_matrix_gen(x_matrix, coeficients)
+        epoch_error.append(MSE(y, xc_pred))
+        print(epoch_error[epoch])
+        """
         if epoch != 0:
-            if (error_change[epoch]-error_change[epoch-1] > threshold):
-                break
-            else:
-                pass
-    
-    epoch+=1
+            print(epoch_error[epoch])
+            #print(str(epoch_error[epoch]-epoch_error[epoch-1]) + ">" + str(threshold))
+            if (epoch_error[epoch]-epoch_error[epoch-1] <= threshold):
+                return coeficients
+        """
+        change = derivatives_errorc(xc_pred, y, x_matrix)
+        coeficients = new_coeficients(coeficients, change, learning_rate)
+        learning_rate = learning_rate/(1+decay)
+        epoch+=1
+
+    return coeficients
         
+
+def xc_matrix_gen(x, c):
+    """
+    xc = []
     
+    for row in range(len(x)):
+        xc.append(sum(x[row]*c))
+    """
+    return np.dot(x,c)
+           
+
+def error_rg(xc_predict, y_true):
+    return ((1/len(y_true)*sum((xc_predict-y_true)**2)))
+            
+
+def derivatives_errorc(xc_predict, y_true, x_ones):
+
+    """
+    error_change = []
+
+    first_term = (2/len(y_true))*sum(xc_predict-y_true)
+
+    sum_columns = [sum(x) for x in zip(*x_ones)]
+
+    for i in range(len(sum_columns)):
+        error_change.append(first_term * sum_columns[i])
+    """
+    #print(xc_predict.shape)
+    #print(y_true.shape)
+    #print(x_ones.shape)
+    #(2/len(xc_predict))*((xc_predict-y_true).transpose()*x_ones).transpose()
+    return (2/len(xc_predict))*np.dot((xc_predict-y_true).transpose(),x_ones).transpose()
+
+def new_coeficients(coeficients, error_rg, eta):
+    """
+    print(coeficients)
+    print("-----------------")
+    print(eta)
+    print("-----------------")
+    print(error_rg)
+    """
+
+    return coeficients - (error_rg *eta)
+
+def main():
+
+    x = pd.read_csv("../data/fish_perch.csv")
+    y = np.array([x['Weight']]).transpose()
+    x.drop(['Weight'], axis=1, inplace = True)
+
+    print(fit(x, y))
 
 
-
+if __name__ == "__main__":
+    main()
+    
+"""
 Y_true = pd.Series([1,2,3,4,5])
   
 Y_pred = pd.Series([0.6,1.29,1.99,2.69,3.4])
 
 MSE(Y_true, Y_pred)
-score(Y_true, Y_pred)
-
-    
-data = pd.read_csv("../data/fish_perch.csv")
-print(data)
-
-data.insert(0, "ones", 1, allow_duplicates=False)
-print(data)
+score(Y_true, Y_pred)"""
