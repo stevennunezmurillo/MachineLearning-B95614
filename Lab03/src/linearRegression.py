cde466809c1edd5fefdc0941ce3624002be0d31f
stevennunezmurillo <snmtra10@gmail.com>
2022-09-28 00:42:21 -0600

    Añadiendo clase de regresion, limpieza de código, modularización

---
+++ Lab03/src/linearRegression.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 22:26:26 2022
+
+@author: Steven Nuñez -B95614
+"""
+
+from xml.sax import SAXNotSupportedException
+
+import predict 
+import random
+import numpy as np
+import pandas as pd
+
+class LinearRegression:
+    
+        
+    def fit(self, x, y, max_epochs=100, threshold=0.01, learning_rate=0.001, momentum=0, decay=0, error='mse', regularization='none', lambdas=0):
+        
+        #Se pasa de dataframe a matriz numpy
+        x_matrix = x.to_numpy()
+        
+        #Creación de coeficientes aleatorios para la primer iteración
+        coeficients = np.array([random.sample(range(len(x_matrix[0])),len(x_matrix[0]))])
+        coeficients = np.transpose(coeficients)
+
+        #Variables locales
+        epoch = 0
+        epoch_error = []
+        list_changes = []
+        
+        #----------------------------------------------------------------------------------
+        #Algoritmo de RG
+        while epoch < max_epochs:
+
+            #Calculo de las predicciones
+            xc_pred = self.xc_matrix_gen(x_matrix, coeficients)
+            
+            #Se selecciona las mejoras para el calculo del error
+            if (regularization == 'l1' or regularization == 'lasso'):
+                current_error = self.lasso(predict.MSE(y, xc_pred), lambdas, coeficients)
+            else:
+                if (regularization == 'l2' or regularization == 'ridge'):
+                    current_error = self.ridge(predict.MSE(y, xc_pred), lambdas, coeficients)
+                else:
+                    current_error = predict.MSE(y, xc_pred)
+                    
+            #Se almacena el error para tener control del error en cada epoca
+            epoch_error.append(current_error)
+            #print(epoch_error[epoch])
+            #Se calcula el cambio, con la derivada
+            change = self.derivatives_errorc(xc_pred, y, x_matrix)
+            
+            #Se almacena para tener controlo de los cambios en las diferentes epocas
+            list_changes.append(change)
+            
+            #Si no es la primer epoca, ver si el cambio en el error entre una época y la anterior sea mayor a un umbral
+            # y se calcula los coeficientes nuevos con el momentum o si es la primer epoca se calcula de manera normal.
+            if epoch != 0:
+
+                if (abs(epoch_error[epoch]-epoch_error[epoch-1]) < threshold):
+                    return coeficients
+                
+                coeficients = self.momentum_coeficients(coeficients, learning_rate, change, momentum, list_changes[epoch-1])
+            else:
+                coeficients = self.new_coeficients(coeficients, change, learning_rate)
+
+            #Manejo del decay
+            learning_rate = learning_rate/(1+decay)
+            
+            #Siguiente epoca
+            epoch+=1
+
+        return coeficients
+            
+    #Calcula el predict de xc
+    def xc_matrix_gen(self, x, c):
+        return np.dot(x,c)
+    
+    #Se calcula el cambio con la derivada respecto a los coeficientes
+    def derivatives_errorc(self, xc_predict, y_true, x_ones):
+        return (2/len(xc_predict))*np.dot((xc_predict-y_true).transpose(),x_ones).transpose()
+
+    #Se calcula los nuevos coeficientes
+    def new_coeficients(self, coeficients, error_rg, eta):
+        return coeficients - (error_rg *eta)
+    
+    #Calculo de los coeficientes con momentum
+    def momentum_coeficients(self, coeficients, eta, change, momentum, last_change):
+        return coeficients - eta * (change + (momentum *last_change))
+        
+    #Mejora para el calculo del error con l1
+    def lasso(self, error_mse, lambdas, coeficients):
+        return error_mse + lambdas + sum(abs(coeficients.transpose()[0]))
+
+    #Mejora para el calculo del error con l2
+    def ridge(self, error_mse, lambdas, coeficients):
+        return error_mse + lambdas + sum((coeficients.transpose()[0])**2)
+
+    #Método para el calculo de predicciones
+    def predict(self, x, coeficients):
+        
+        x_matrix = x.to_numpy()
+        return np.dot(x_matrix, coeficients)
+        
+
+
+
--- Lab03/src/predict.py
+++ Lab03/src/predict.py
@@ -1,12 +1,15 @@
 from msilib.schema import ServiceInstall
+from ssl import _create_default_https_context
 from tkinter import Y
 from xml.sax import SAXNotSupportedException
-import numpy as np
-import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-import random
 
+import numpy as np
+import pandas as pd
+
+
+#Método de Mínimos cuadrados, error cuadrático medio.
 def MSE(y_true, y_predict):
     
     error = 0
@@ -17,118 +20,21 @@ def MSE(y_true, y_predict):
     error *= 1/ len(y_true)
     
     return error
-    #mse = mean_squared_error(y_true, y_predict)
     
-  
+
+#Método de Coeficiente de determinación (R2) 
 def score(y_true, y_predict):
     
-    y_true.to_numpy()
-    y_predict.to_numpy()
+    #r2 = r2_score(y_true, y_predict)
+
     
-    first_sum = sum(((y_true - y_predict)**2))
-    mean = y_true.mean()
-    second_sum = sum(((y_true - mean)**2))
+    first_sum = sum(((y_true[0] - y_predict[0])**2))
+    mean = y_true[0].mean()
+    second_sum = sum(((y_true[0] - mean)**2))
     
     score_r2 = (second_sum-first_sum)/second_sum
-    #r2 = r2_score(y_true, y_predict) 
-        
-    #print(score_r2)
-
-def fit(x, y, max_epochs=100, threshold=0.01, learning_rate=1e-7, momentum=0, decay=0, error='mse', regularization='none', lambdas = 0):
     
-    #Se agrega el bias  
-    x.insert(0, "ones", 1, allow_duplicates=False)
-
-    #Se pasa de dataframe a matriz numpy
-    x_matrix = x.to_numpy()
-    coeficients = np.array([random.sample(range(len(x_matrix[0])),len(x_matrix[0]))])
-    epoch = 0
-    epoch_error = []
-    
-    coeficients = np.transpose(coeficients)
-    
-    #Algoritmo de RG
-    while epoch < max_epochs:
-
-        xc_pred = xc_matrix_gen(x_matrix, coeficients)
-        epoch_error.append(MSE(y, xc_pred))
-        print(epoch_error[epoch])
-        """
-        if epoch != 0:
-            print(epoch_error[epoch])
-            #print(str(epoch_error[epoch]-epoch_error[epoch-1]) + ">" + str(threshold))
-            if (epoch_error[epoch]-epoch_error[epoch-1] <= threshold):
-                return coeficients
-        """
-        change = derivatives_errorc(xc_pred, y, x_matrix)
-        coeficients = new_coeficients(coeficients, change, learning_rate)
-        learning_rate = learning_rate/(1+decay)
-        epoch+=1
-        
-    print("-----------------")
-
-    return coeficients
+    return r2_score(y_true[0], y_predict[0])
+     
         
 
-def xc_matrix_gen(x, c):
-    """
-    xc = []
-    
-    for row in range(len(x)):
-        xc.append(sum(x[row]*c))
-    """
-    return np.dot(x,c)
-           
-
-def error_rg(xc_predict, y_true):
-    return ((1/len(y_true)*sum((xc_predict-y_true)**2)))
-            
-
-def derivatives_errorc(xc_predict, y_true, x_ones):
-
-    """
-    error_change = []
-
-    first_term = (2/len(y_true))*sum(xc_predict-y_true)
-
-    sum_columns = [sum(x) for x in zip(*x_ones)]
-
-    for i in range(len(sum_columns)):
-        error_change.append(first_term * sum_columns[i])
-    """
-    #print(xc_predict.shape)
-    #print(y_true.shape)
-    #print(x_ones.shape)
-    #(2/len(xc_predict))*((xc_predict-y_true).transpose()*x_ones).transpose()
-    return (2/len(xc_predict))*np.dot((xc_predict-y_true).transpose(),x_ones).transpose()
-
-def new_coeficients(coeficients, error_rg, eta):
-    """
-    print(coeficients)
-    print("-----------------")
-    print(eta)
-    print("-----------------")
-    print(error_rg)
-    """
-
-    return coeficients - (error_rg *eta)
-
-def main():
-
-    x = pd.read_csv("../data/fish_perch.csv")
-    y = np.array([x['Weight']]).transpose()
-    x.drop(['Weight'], axis=1, inplace = True)
-    
-    print(fit(x, y))
-
-
-if __name__ == "__main__":
-    main()
-    
-"""
-Y_true = pd.Series([1,2,3,4,5])
-  
-Y_pred = pd.Series([0.6,1.29,1.99,2.69,3.4])
-
-MSE(Y_true, Y_pred)
-score(Y_true, Y_pred)"""
