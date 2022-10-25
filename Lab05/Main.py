e1d7a99e00da411cfea2633546ac2f84815da8d4
Steven Mauricio Nunez Murillo <B95614@ecci.ucr.ac.cr>
2022-10-25 15:56:07 -0600

    Falta optimización de parametros

--- Lab05/Main.py
+++ Lab05/Main.py
@@ -4,6 +4,8 @@ import torch
 from torch import nn
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sn
+import pandas as pd
 
 #Verificando si torch.cuda está disponible
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,22 +26,63 @@ convNN = ConvNN()
 lossFunction = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(convNN.parameters(),0.001)
 
-# Entrenamiento del modelo
-for epoch in range(0, 250):
-    # Training
-        
-    optimizer.zero_grad()
-    pred = convNN(x_train)
-    print(pred.shape)
-    print(y_train.shape)
-    loss = lossFunction(pred, y_train.squeeze())
-    loss.backward()
-    optimizer.step()
-    print(loss)
+entrenado = False
 
-    # Validation
-    with torch.no_grad():
-       loss2 = lossFunction( convNN(x_test), y_test.squeeze())
-       
-    loss_train.append(loss.item())
-    loss_test.append(loss2.item())
+if entrenado == False:
+    
+    convNN.train()
+    # Entrenamiento del modelo
+    for epoch in range(0, 100):
+        
+        x_train.to(device)
+        y_train.to(device)
+        
+        # Training
+            
+        optimizer.zero_grad()
+        pred = convNN(x_train)
+        print(pred.shape)
+        print(y_train.shape)
+        loss = lossFunction(pred, y_train.squeeze())
+        loss.backward()
+        optimizer.step()
+        print(loss)
+    
+        # Validation
+        with torch.no_grad():
+            
+           x_test.to(device)
+           y_test.to(device) 
+            
+           loss2 = lossFunction( convNN(x_test), y_test.squeeze())
+           
+        loss_train.append(loss.item())
+        loss_test.append(loss2.item())
+        
+        if epoch % 50 == 0:
+               torch.save(convNN.state_dict(), f'convNNEpoca{epoch}.pt')
+
+else:
+    convNN.load_state_dict(torch.load('Model_final.pt'))
+    
+
+
+y_true = []
+y_pred = []
+
+y_pred_out = convNN(x_test)
+
+y_pred_out = (torch.max(torch.exp(y_pred_out), 1)[1]).data.cpu().numpy()
+y_pred.extend(y_pred_out) # Save Prediction
+    
+y_test = y_test.data.cpu().numpy()
+y_true.extend(y_test) # Save Truth
+    
+
+# Build confusion matrix
+cf_matrix = confusion_matrix(y_true, y_pred)
+df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix) *10, index = [i for i in names],
+                     columns = [i for i in names])
+plt.figure(figsize = (12,7))
+sn.heatmap(df_cm, annot=True)
+plt.savefig('output.png')
